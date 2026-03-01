4b8c48e587b596b395de244a7f703c2ef64be3df
Abdelhakar7 <abdelhakseghiour59@gmail.com>
2026-03-01 17:26:11 +0100

    h

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
-# main.py
-
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 from app.routes.router import router as chat_router
 
 app = FastAPI(
@@ -11,3 +11,9 @@ app = FastAPI(
 
 # Include your chat router
 app.include_router(chat_router, prefix="/chat", tags=["Chat"])
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+
+@app.get("/", include_in_schema=False)
+def root():
+    return FileResponse("app/static/index.html")
